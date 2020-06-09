4740c3e16e1182bbcc58a009d80b2d309f1a0f76
xiebing0217 <327660381@qq.com>
2020-06-09 17:15:21 +0800

    第一次提交

---
+++ git-pro/src/com/nnxy/user/User.java
@@ -0,0 +1,17 @@
+package com.nnxy.user;
+
+public class User {
+
+	public void addUser(int id,String username,String password) {
+		//add
+	}
+	
+	public void deleteUser(int id) {
+		//add
+	}
+	
+	public void updateUser(int id,String username,String password) {
+		//add
+	}
+	
+}
